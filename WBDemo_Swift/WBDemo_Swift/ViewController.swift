be7e3e8d643914a4c9e279f6123a21d8c9e67707
peter <1176175468@qq.com>
2016-12-06 14:30:36 +0800

    项目初步搭建

---
+++ WBDemo_Swift/WBDemo_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WBDemo_Swift
+//
+//  Created by peter on 16/12/6.
+//  Copyright © 2016年 peter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
